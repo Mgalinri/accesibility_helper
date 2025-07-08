3f095c201327fa06b9ee7c2263669e9b83683fdd
Mgalinri <galindom@jbu.edu>
2025-07-08 15:21:42 -0500

    Actual changes

--- accesibility-helper/extension.js
+++ accesibility-helper/extension.js
@@ -10,6 +10,7 @@ const vscode = require('vscode');
  */
 
 //To-Do: Handle if the link has a <> characters
+//To-Do: Regex into variables
 function activate(context) {
 
 	console.log('Congratulations, your extension "accesibility-helper" is now active!');
@@ -21,6 +22,7 @@ function activate(context) {
 		
         function findImageTags(active_window){
 			/* Finds all of the html img tags */
+			//To-Do: Clean this function for SOLID code
             if (active_window) {
 			  text = active_window.document.getText().match(/<img[\S\s]*?>/g)
          	  return text||[]
@@ -29,27 +31,48 @@ function activate(context) {
 			  return []
         	}
 		}
-		
-		function replaceAlts(image_elements_array){
+
+		function replaceAlts(image_elements_array,edited_text){
            image_elements_array.forEach((element,index) => {
 			    
 			if(element.includes(" alt=")||element.includes(" alt ="))
 			{
-                image_elements_array[index] = element.replace(/\salt\s?=\s?["'].*["']/, ' alt = "no source"')
+                edited_text.append(element.replace(/\salt\s?=\s?["'].*["']/, ' alt = "no source"'))
 			}
 			else
 			{
-			   image_elements_array[index] = element.replace(/\/?>/, ' alt = "no source">')
+			   edited_text.append(element.replace(/\/?>/, ' alt = "no source">'))
 			}
 				
 		   });
 		   
-               return image_elements_array
+               return edited_text
 		}
 
+		function addEditstoOriginalText(og_text,og_fragments,edited_fragments){
+			og_fragments.forEach((element,index)=>{
+				
+				og_text = og_text.replace(element,edited_fragments[index])
+
+			})
+             
+		}
+        
+		function replaceDocument(active_window){
+			const lastLine = active_window.document.lineCount-1
+            const start = vscode.Position(0,0)
+			const end = vscode.Position(lastLine,active_window.document.lineAt(lastLine).getText().length())
+			const range = vscode.Range(start,end)
+			current_active_window.document.replace(range,edited_text)
+		}   
+
 		const current_active_window = vscode.window.activeTextEditor;
-		let original_text = findImageTags(current_active_window);
-		original_text = replaceAlts(original_text);
+		let og_text = current_active_window.document.getText()
+		const og_text_img_tags =findImageTags(current_active_window);
+		let edited_text;
+		replaceAlts(og_text_img_tags,edited_text);
+		addEditstoOriginalText(og_text,og_text_img_tags,edited_text)
+
 		
 		
 	
